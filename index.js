25dc704cee5c7a59146bd094fc8c2f262f0a66c3
Carla <ruixdsgn@gmail.com>
2023-05-06 15:57:41 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const submitData = (name, email) => {
+    const postData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name, email})
+    }
+
+   return fetch("http://localhost:3000/users", postData)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (object) {
+           document.body.innerHTML = object['id']
+        })
+
+        .catch(function (error) {
+            console.log(error.message)
+            document.body.innerHTML = error['message']
+        })
+}
+
+document.addEventListener("DOMContentLoaded", submitData);
+
+
+
